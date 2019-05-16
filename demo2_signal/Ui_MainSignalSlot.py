b7d4c35b4d9496ce46cff0b0b215b19c77995a1c
weiy <wei.yuan@uisee.com>
2019-05-16 16:00:36 +0800

    add signal demo

---
+++ demo2_signal/Ui_MainSignalSlot.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '/home/yarlen/Test/qt/demo2_signal/MainSignalSlot.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(1080, 677)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(340, 310, 99, 27))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        self.pushButton.clicked.connect(Form.close)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "关闭窗口"))
+
+
