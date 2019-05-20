af53a6e6d353f7d2560334ed195420a439db8f72
weiy <wei.yuan@uisee.com>
2019-05-20 10:41:00 +0800

    add menu demo

---
+++ demo4_menu/Ui_menu.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '/home/yarlen/Test/qt/demo3_menu/menu.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(810, 621)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 810, 31))
+        self.menubar.setObjectName("menubar")
+        self.menu = QtWidgets.QMenu(self.menubar)
+        self.menu.setObjectName("menu")
+        self.menu_2 = QtWidgets.QMenu(self.menubar)
+        self.menu_2.setObjectName("menu_2")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.toolBar = QtWidgets.QToolBar(MainWindow)
+        self.toolBar.setObjectName("toolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        self.action = QtWidgets.QAction(MainWindow)
+        self.action.setObjectName("action")
+        self.action_2 = QtWidgets.QAction(MainWindow)
+        self.action_2.setObjectName("action_2")
+        self.action_3 = QtWidgets.QAction(MainWindow)
+        self.action_3.setObjectName("action_3")
+        self.addWinAction = QtWidgets.QAction(MainWindow)
+        self.addWinAction.setObjectName("addWinAction")
+        self.menu.addAction(self.action)
+        self.menu.addAction(self.action_2)
+        self.menu.addAction(self.action_3)
+        self.menubar.addAction(self.menu.menuAction())
+        self.menubar.addAction(self.menu_2.menuAction())
+        self.toolBar.addAction(self.addWinAction)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menu.setTitle(_translate("MainWindow", "文件"))
+        self.menu_2.setTitle(_translate("MainWindow", "编辑"))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
+        self.action.setText(_translate("MainWindow", "打开"))
+        self.action.setShortcut(_translate("MainWindow", "Ctrl+O"))
+        self.action_2.setText(_translate("MainWindow", "新建"))
+        self.action_2.setShortcut(_translate("MainWindow", "Ctrl+N"))
+        self.action_3.setText(_translate("MainWindow", "关闭"))
+        self.action_3.setShortcut(_translate("MainWindow", "Ctrl+C"))
+        self.addWinAction.setText(_translate("MainWindow", "添加窗体"))
+        self.addWinAction.setToolTip(_translate("MainWindow", "添加窗体"))
+
+
